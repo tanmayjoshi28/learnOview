9e2101437c758547099195149a4556fa58a8f0b9
Tanmay Joshi <53273317+tanmayjoshi28@users.noreply.github.com>
2020-05-29 18:56:47 +0530

    Setting up Email-Backend

--- video_player/settings.py
+++ video_player/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'crispy_forms',
     'embed_video',
     'users.apps.UsersConfig',
     'videoCategories.apps.VideocategoriesConfig',
@@ -126,6 +127,17 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'git.pineapple@gmail.com'
+EMAIL_HOST_PASSWORD = 'vitchennai'
+PASSWORD_RESET_TIMEOUT_DAYS = 1
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+
 
 
 
