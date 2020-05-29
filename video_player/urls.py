1c79385417490ecea416227413f08f260909cf7b
Tanmay Joshi <53273317+tanmayjoshi28@users.noreply.github.com>
2020-05-29 18:48:11 +0530

    Urls for Forget Password

--- video_player/urls.py
+++ video_player/urls.py
@@ -19,10 +19,27 @@ from django.urls import path, include
 from users.views import startPage
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('videoCategories/', include('videoCategories.urls')),
-    url(r'^$', startPage, name='start')
+    path('', startPage, name='startpage'),
+
+    path('reset_password/',
+         auth_views.PasswordResetView.as_view(
+             template_name='users/password_reset.html'),
+         name='reset_password'),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(
+            template_name='users/password_reset_sent.html'),
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+            template_name='users/new_password_set.html'),
+         name='password_reset_confirm'),
+    path('reset_password/complete/', auth_views.PasswordResetCompleteView.as_view(
+            template_name='users/reset_complete.html'),
+         name='password_reset_complete')
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
