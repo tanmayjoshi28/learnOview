78d482d3490e71727f49a9b7800ed3a1468ec874
Tanmay Joshi <53273317+tanmayjoshi28@users.noreply.github.com>
2020-06-01 16:12:20 +0530

    Logout-url

--- users/urls.py
+++ users/urls.py
@@ -7,4 +7,5 @@ app_name = 'users'
 urlpatterns = [
     url(r'^login/$', Login, name='login'),
     url(r'^register/$', Register, name='register'),
+    url(r'^logout/$', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout')
 ]
